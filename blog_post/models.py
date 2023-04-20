31a9bc13a27955a8e1f82fbb74ea29deb8275ca9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-20 16:18:04 -0300

    adiciona DB mysql

--- blog_post/models.py
+++ blog_post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -9,6 +10,7 @@ class BlogPost(models.Model):
     published = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField(Category)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.title
