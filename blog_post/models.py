a9073eeebd64050e4930188f13ebb810a06b9906
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-20 15:27:27 -0300

    adiciona relacionamento NxN entre post e category

--- blog_post/models.py
+++ blog_post/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from category.models import Category
 # Create your models here.
 
 
@@ -8,3 +8,7 @@ class BlogPost(models.Model):
     content = models.TextField()
     published = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
